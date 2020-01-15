bf8aad68d8849c8ce78ee53baff598a2e06d9a3f
Anurag Srivastava <anurag.anu156@gmail.com>
2020-01-15 15:49:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,31 +15,31 @@ def get_delay():
     if request.method == 'POST':
         result = request.form
 
-#         # Prepare the feature vector for prediction
-#         pkl_file = open('cat', 'rb')
-#         index_dict = pickle.load(pkl_file)
-#         new_vector = np.zeros(len(index_dict))
+        # Prepare the feature vector for prediction
+        pkl_file = open('cat', 'rb')
+        index_dict = pickle.load(pkl_file)
+        new_vector = np.zeros(len(index_dict))
 
-#         try:
-#             new_vector[index_dict['DayOfWeek' + str(result['DayOfWeek'])]] = 1
-#         except:
-#             pass
-#         try:
-#             new_vector[index_dict['UniqueCarrier' + str(result['UniqueCarrier'])]] = 1
-#         except:
-#             pass
-#         try:
-#             new_vector[index_dict['Origin' + str(result['Origin'])]] = 1
-#         except:
-#             pass
-#         try:
-#             new_vector[index_dict['Dest' + str(result['Dest'])]] = 1
-#         except:
-#             pass
-#         try:
-#             new_vector[index_dict['DepTime' + str(result['DepTime'])]] = 1
-#         except:
-#             pass
+        try:
+            new_vector[index_dict['DayOfWeek' + str(result['DayOfWeek'])]] = 1
+        except:
+            pass
+        try:
+            new_vector[index_dict['UniqueCarrier' + str(result['UniqueCarrier'])]] = 1
+        except:
+            pass
+        try:
+            new_vector[index_dict['Origin' + str(result['Origin'])]] = 1
+        except:
+            pass
+        try:
+            new_vector[index_dict['Dest' + str(result['Dest'])]] = 1
+        except:
+            pass
+        try:
+            new_vector[index_dict['DepTime' + str(result['DepTime'])]] = 1
+        except:
+            pass
 
         pkl_file = open('logmodel.pkl', 'rb')
         logmodel = pickle.load(pkl_file)
